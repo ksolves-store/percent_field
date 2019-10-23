bebdd2f3d0baf18e50e365d181a1fa87db79cb97
ksolves-store-dev <55271205+ksolves-store-dev@users.noreply.github.com>
2019-10-23 19:09:16 +0530

    13.0 stage (#1)
    * percent-field v13.0 completed.
    
    * Updated Description File

--- ks_percent_field/__manifest__.py
+++ ks_percent_field/__manifest__.py
@@ -2,23 +2,23 @@
     'name': "Percent Field",
 
     'summary': """
-        Percent Field v12.0""",
+        Percent Field v13.0""",
 
     'description': """
-              Percent field for odoo 12.0. 
+              Percent field for odoo 13.0. 
       """,
     'author': "Ksolves India Pvt. Ltd.",
     'website': "https://www.ksolves.com/",
     'license': 'LGPL-3',
-    'live_test_url':'https://youtu.be/Guuyj7Gns00',
+    'live_test_url': 'https://youtu.be/Guuyj7Gns00',
     'category': 'tools',
     'support': 'sales@ksolves.com',
-    'version': '1.0.1',
+    'version': '1.0.0',
     'images': [
         'static/description/main.jpg',
     ],
     # any module necessary for this one to work correctly
-    'depends': ['base','web'],
+    'depends': ['base', 'web'],
 
     # always loaded
     'data': [
