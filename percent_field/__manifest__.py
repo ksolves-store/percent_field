90d9b043e8b5f075cce5f4585bb26b3465a55d6c
kajal.ksolves <kajal.rani@ksolves.com>
2019-10-24 11:08:42 +0530

    App name: percent_field

---
+++ percent_field/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': "Percent Field",
+
+    'summary': """
+        Percent Field v13.0""",
+
+    'description': """
+              Percent field for odoo 13.0. 
+      """,
+    'author': "Ksolves India Pvt. Ltd.",
+    'website': "https://www.ksolves.com/",
+    'license': 'LGPL-3',
+    'live_test_url': 'https://youtu.be/Guuyj7Gns00',
+    'category': 'tools',
+    'support': 'sales@ksolves.com',
+    'version': '1.0.0',
+    'images': [
+        'static/description/main.jpg',
+    ],
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'web'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/assets.xml',
+
+    ],
+}
+# -*- coding: utf-8 -*-
